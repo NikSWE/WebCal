e50b9dc3439eebe885fb486c30a0524c9ead8a3a
Nikhil Anand <nikhil_anand@outlook.com>
2019-03-13 12:01:08 +0530

    logic change

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
 $( document ).ready( function () {
-    var cal_display_value = $("#cal_display").val();
+
+    // calculate sum of operands
+    let add = function ( a, b ) {
+        return a + b;
+    }
+
+    // calculate difference of operands
+    let subtract = function ( a, b ) {
+        return a - b;
+    }
+
+    // calculate product of operands
+    let multiply = function ( a, b ) {
+        return a * b;
+    }
+
+    // calculate quotient of operands
+    let divide = function ( a, b ) {
+        return a / b;
+    }
+
+    // calculate remainder of operands
+    let modulo = function ( a, b ) {
+        return a % b;
+    }
+
+    // array which contains user's expression
+    var cal_display_value = [];
+
+    // button listener
     $( "button" ).click( function () {
-        cal_display_value = $(this).val();
-        console.log(cal_display_value);
-        $("#cal_display").val("hey changed the value");
+        let value = $( this ).val();
+        
+        // cal_display_value.push(value);
     } );
 } );
