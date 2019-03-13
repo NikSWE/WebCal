d1441fce3b6130c72b601506836022589762d0f2
Nikhil Anand <nikhil_anand@outlook.com>
2019-03-13 15:42:07 +0530

    error in for loop

--- js/main.js
+++ js/main.js
@@ -57,6 +57,47 @@ $( document ).ready( function () {
         return flag;
     }
 
+    // evaluate userExpression
+    let evaluate = function ( array ) {
+        let operand1, operand2, operator, result;
+        for ( let i = 0; i < array.length; i = i + 3 ) {
+            console.log( `${i} = ${array[i]},${array[i+1]},${array[i+2]} ` )
+            operand1 = array[ i ];
+            operator = array[ i + 1 ];
+            if ( operator === '+/-' ) {
+                operand2 = '-1';
+            } else {
+                operand2 = array[ i + 2 ];
+            }
+            switch ( operator ) {
+                case '+':
+                    result = add( operand1, operand2 );
+                    break;
+                case '-':
+                    result = subtract( operand1, operand2 );
+                    break;
+                case '*':
+                    result = multiply( operand1, operand2 );
+                    break;
+                case '/':
+                    result = divide( operand1, operand2 );
+                    break;
+                case '%':
+                    result = modulo( operand1, operand2 );
+                    break;
+                case '+/-':
+                    result = multiply( operand1, operand2 );
+                    break;
+                default:
+                    break;
+            }
+            array.splice( i, 3, result );
+            console.log(i);
+            
+        }
+        return array;
+    }
+
     // button listener
     $( "button" ).click( function () {
         // obtain the value of the btn
@@ -94,7 +135,8 @@ $( document ).ready( function () {
                 }
             }
         } else if ( value === '=' ) {
-            console.log( '= btn pressed' );
+            // evaluate the userExpression array
+            userExpression = evaluate( userExpression );
         } else if ( userExpression === undefined || userExpression.length == 0 ) {
             // array empty or does not exist
             // check if value is an operator
