c451eaaa13615bbbd7e25fc49143c6b0ce229657
Nikhil Anand <nikhil_anand@outlook.com>
2019-03-13 14:53:01 +0530

    updated the main.js

--- js/main.js
+++ js/main.js
@@ -26,12 +26,54 @@ $( document ).ready( function () {
     }
 
     // array which contains user's expression
-    var cal_display_value = [];
+    let userExpression = [];
+    console.log( userExpression );
+
+
+    // array which contains all the valid operators
+    // performed by the calculator
+    let operatorList = [ '+', '-', '*', '/', '%', '+/-' ];
+
+    // check if value is an operator
+    // if an operator return true
+    // else return false
+    let isOperator = function ( array, value ) {
+        let flag = false;
+        array.forEach( function ( operator ) {
+            if ( value === operator ) {
+                flag = true;
+            }
+        } );
+        return flag;
+    }
 
     // button listener
     $( "button" ).click( function () {
         let value = $( this ).val();
-        
-        // cal_display_value.push(value);
+
+        if ( userExpression === undefined || userExpression.length == 0 ) {
+            // array empty or does not exist
+            // push the value into the array
+            userExpression.push( value );
+        } else if ( isOperator( operatorList, value ) ) {
+            // check if value of the btn
+            // is an operator if 'yes'
+            // push into the array
+            userExpression.push( value );
+        } else {
+            // check value at last index of userExpression array
+            // if it is an operator then push the value
+            if ( isOperator( operatorList, userExpression[ userExpression.length - 1 ] ) ) {
+                userExpression.push( value );
+            }
+            // else pop prev value and concat
+            // with new value push it back afterwards
+            else {
+                let temp = userExpression.pop();
+                temp += value;
+                userExpression.push( temp );
+            }
+        }
+        console.log( userExpression );
     } );
 } );
