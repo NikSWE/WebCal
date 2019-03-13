4ad8234a6b8f6e061e975de7370224d38f380c71
Nikhil Anand <nikhil_anand@outlook.com>
2019-03-13 15:14:34 +0530

    fixed all the buttons

--- js/main.js
+++ js/main.js
@@ -2,27 +2,37 @@ $( document ).ready( function () {
 
     // calculate sum of operands
     let add = function ( a, b ) {
-        return a + b;
+        a = Number( a );
+        b = Number( b );
+        return `${a + b}`;
     }
 
     // calculate difference of operands
     let subtract = function ( a, b ) {
-        return a - b;
+        a = Number( a );
+        b = Number( b );
+        return `${a - b}`;
     }
 
     // calculate product of operands
     let multiply = function ( a, b ) {
-        return a * b;
+        a = Number( a );
+        b = Number( b );
+        return `${a * b}`;
     }
 
     // calculate quotient of operands
     let divide = function ( a, b ) {
-        return a / b;
+        a = Number( a );
+        b = Number( b );
+        return `${a / b}`;
     }
 
     // calculate remainder of operands
     let modulo = function ( a, b ) {
-        return a % b;
+        a = Number( a );
+        b = Number( b );
+        return `${a % b}`;
     }
 
     // array which contains user's expression
@@ -49,12 +59,51 @@ $( document ).ready( function () {
 
     // button listener
     $( "button" ).click( function () {
+        // obtain the value of the btn
         let value = $( this ).val();
 
-        if ( userExpression === undefined || userExpression.length == 0 ) {
+        if ( value === 'AC' ) {
+            // clear the userExpression array
+            userExpression = [];
+        } else if ( value === 'DEL' ) {
+            // check if userExpression array isEmpty
+            // if yes print error on display
+            if ( userExpression === undefined || userExpression.length == 0 ) {
+                console.log( 'Error on display' );
+            }
+            // else if check if value at
+            // last index is an operator
+            // if yes pop the value
+            else if ( isOperator( operatorList, userExpression[ userExpression.length - 1 ] ) ) {
+                userExpression.pop();
+            }
+            // else delete the last input
+            else {
+                let temp = userExpression[ userExpression.length - 1 ];
+                // check if the length of temp
+                // is equal to 1 if yes then
+                // pop the last value from userExpression
+                if ( temp.length == 1 ) {
+                    userExpression.pop();
+                }
+                // else slice the last value from temp
+                // and push it back in userExpression
+                else {
+                    temp = temp.slice( 0, -1 );
+                    userExpression.splice( userExpression.length - 1, 1, temp );
+                }
+            }
+        } else if ( userExpression === undefined || userExpression.length == 0 ) {
             // array empty or does not exist
-            // push the value into the array
-            userExpression.push( value );
+            // check if value is an operator
+            // if yes print error on display
+            if ( isOperator( operatorList, value ) ) {
+                console.log( 'Error on display' );
+            }
+            // else push the value into the array
+            else {
+                userExpression.push( value );
+            }
         } else if ( isOperator( operatorList, value ) ) {
             // check if value of the btn
             // is an operator if 'yes'
